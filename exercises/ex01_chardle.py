12b90b26729069a58805fb163c867926a776a0e2
Kailey Patel <kailp@ad.unc.edu>
2022-01-23 21:06:24 -0500

    Progress on Exercise 1!!

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -2,36 +2,43 @@
 
 __author__ = "730523395"
 
-guess: str = input("Enter a 5-character word: ")
 
-if len(guess) != int(5):
+GUESS: str = input("Enter a 5-character word: ")
+if len(GUESS) != int(5):
     exit("Error: Word must contain 5 characters")
 
-guess2: str = input("Enter a single character: ")
-if len(guess2) != int(1):
+
+GUESS2: str = input("Enter a single character: ")
+if len(GUESS2) != int(1):
     exit("Error: Character must be a single character.")
 else:
-    print("searching for " + guess2 + " in " + guess)
+    print("Searching for " + GUESS2 + " in " + GUESS)
+
 
 count_word = 0
 
-if guess[0] == guess2:
-    print(guess2 + " found at index 0 ")
-    count_word += 1
-if guess[1] == guess2:
-    print(guess2 + " found at index 1 ")
-    count_word += 1
-if guess[2] == guess2:
-    print(guess2 + " found at index 2 ")
-    count_word += 1
-if guess[3] == guess2:
-    print(guess2 + " found at index 3 ")
-    count_word += 1
-if guess[4] == guess2:
-    print(guess2 + " found at index 4 ")
-    count_word += 1
 
-if count_word == 0: 
-    print("No instances of " + guess2 + " found in " + guess) 
-if count_word >= 1:
-    print(count_word, " instance of " + guess2 + " found in " + guess)
+if GUESS[0] == GUESS2:
+    print(GUESS2 + " found at index 0 ")
+    count_word += 1
+if GUESS[1] == GUESS2:
+    print(GUESS2 + " found at index 1 ")
+    count_word += 1
+if GUESS[2] == GUESS2:
+    print(GUESS2 + " found at index 2 ")
+    count_word += 1
+if GUESS[3] == GUESS2:
+    print(GUESS2 + " found at index 3 ")
+    count_word += 1
+if GUESS[4] == GUESS2:
+    print(GUESS2 + " found at index 4 ")
+    count_word += 1
+    
+    
+if count_word == 1:
+    print(count_word, "instance of " + GUESS2 + " found in " + GUESS)
+if count_word > 1:
+    print(count_word, "instances of " + GUESS2 + " found in " + GUESS)
+else:
+    if count_word == 0: 
+        print("No instances of " + GUESS2 + " found in " + GUESS) 
