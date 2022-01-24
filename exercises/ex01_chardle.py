fa056cfdf9ef9ab9f55b4e9c71559459ccaeff67
Kailey Patel <kailp@ad.unc.edu>
2022-01-23 20:30:19 -0500

    Progress on Exercise 1!

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -4,14 +4,14 @@ __author__ = "730523395"
 
 guess: str = input("Enter a 5-character word: ")
 
-if len(guess) != len("str!!"):
+if len(guess) != int(5):
     exit("Error: Word must contain 5 characters")
 
 guess2: str = input("Enter a single character: ")
-print("searching for " + guess2 + " in " + guess)
-
-if len(guess2) != len("s"):
+if len(guess2) != int(1):
     exit("Error: Character must be a single character.")
+else:
+    print("searching for " + guess2 + " in " + guess)
 
 count_word = 0
 
@@ -33,7 +33,5 @@ if guess[4] == guess2:
 
 if count_word == 0: 
     print("No instances of " + guess2 + " found in " + guess) 
-if count_word == 1:
-    print("1 instance of " + guess2 + " found in " + guess)
-if count_word > 1: 
-    print("2 instances of " + guess2 + " found in " + guess)
+if count_word >= 1:
+    print(count_word, " instance of " + guess2 + " found in " + guess)
